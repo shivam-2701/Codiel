d3f337a8f45acb05d0edaf8ebc8fc1a409138e05
shivam-2701 <shivam.shah2701@gmail.com>
2023-03-14 19:03:37 +0530

    Post deletion added

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = (req, res) => {
   Post.create({
@@ -11,5 +12,29 @@ module.exports.create = (req, res) => {
     })
     .catch((error) => {
       console.log("Error creating post in post_controller", error);
+    });
+};
+
+module.exports.destroy = (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      // .id means converting the object id to a string
+      if (post.user.toString() == req.user.id) {
+        post.remove();
+
+        Comment.deleteMany({ post: req.params.id })
+          .then(() => {
+            return res.redirect("back");
+          })
+          .catch((err) => {
+            console.log("Error in deleting comment", err);
+            return res.redirect("back");
+          });
+      }else{
+        return res.redirect('back');
+      }
+    })
+    .catch((error) => {
+      console.log("Error in finding the Post {post_controller}", error);
     });
 };
