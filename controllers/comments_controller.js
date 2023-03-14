006d8e73aab3cd9fac19106ab4939ead8e460ef6
shivam-2701 <shivam.shah2701@gmail.com>
2023-03-14 19:34:07 +0530

    Comment deletion added

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -28,3 +28,26 @@ module.exports.create = function (req, res) {
       res.redirect('back');
     });
 }; 
+
+// Comment Deltetion action
+
+module.exports.destroy = (req,res)=>{
+
+  Comment.findById(req.params.id)
+  .then((comment)=>{
+      if(comment && comment.user== req.user.id){
+
+        Comment.findByIdAndDelete(req.params.id).then(()=>{
+          res.redirect('back');
+        }).catch((err)=>{
+          console.log('Error while deleting the comment', err);
+          res.redirect('back');
+        });
+      }else{
+        res.redirect('back');
+      }
+  })
+  .catch((error)=>{
+      console.log("Error in find the comment {comment_controller.destroy}", error);
+  })
+};
